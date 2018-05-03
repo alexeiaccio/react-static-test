509a299fd92dbaa495e9352713b3c00caed59eb2
Alexei Accio <32056666+alexeiaccio@users.noreply.github.com>
2018-05-03 14:40:35 +0300

    fix bug with title

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -11,7 +11,7 @@ export default withRouteData(({ posts }) => (
     <ul>
       {posts.map(post => (
         <li key={post.id}>
-          <Link to={`/blog/post/${post.id}/`}>{post.title}</Link>
+          <Link to={`/blog/post/${post.id}/`}>{post.title[0].text}</Link>
         </li>
       ))}
     </ul>
